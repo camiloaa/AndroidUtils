23f2a5cf5bdb3649f81cc7a2f065fcd6f95d9f1c
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2012-12-30 20:11:59 +0100

    Added a ComplexTransition class

---
+++ src/com/github/androidutils/statemachine/ComplexTransition.java
@@ -0,0 +1,22 @@
+package com.github.androidutils.statemachine;
+
+import android.os.Message;
+
+public abstract class ComplexTransition extends State {
+    abstract public void performComplexTransition();
+
+    @Override
+    public final void enter() {
+        performComplexTransition();
+    }
+
+    @Override
+    public boolean processMessage(Message msg) {
+        throw new RuntimeException("performComplexTransition() must transit immediately");
+    }
+
+    @Override
+    public final void exit() {
+        // nothing to do
+    }
+}
