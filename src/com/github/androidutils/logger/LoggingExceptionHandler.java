160b939da8bde0684d056f59f7718e2a82fd70dd
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2013-02-03 23:10:19 +0100

    Added LoggingExceptionHandler
    	new file:   src/com/github/androidutils/logger/LoggingExceptionHandler.java

---
+++ src/com/github/androidutils/logger/LoggingExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.github.androidutils.logger;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+public class LoggingExceptionHandler implements UncaughtExceptionHandler {
+
+    private final Logger logger;
+
+    UncaughtExceptionHandler previousExceptionHandler;
+
+    public static void addLoggingExceptionHandlerToAllThreads(Logger logger) {
+        Thread.setDefaultUncaughtExceptionHandler(new LoggingExceptionHandler(logger, Thread
+                .getDefaultUncaughtExceptionHandler()));
+    }
+
+    public LoggingExceptionHandler(Logger logger, UncaughtExceptionHandler previousExceptionHandler) {
+        this.logger = logger;
+        this.previousExceptionHandler = previousExceptionHandler;
+    }
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+        logger.e("Unhandled exception in " + thread.toString(), ex);
+        previousExceptionHandler.uncaughtException(thread, ex);
+    }
+
+}
