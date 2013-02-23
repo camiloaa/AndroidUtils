c87981c02adc42b97267952ea4858c474406117a
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2013-02-23 20:23:51 +0100

    revert changes to complexTranslation - bug is fixed, no need for logging

--- src/com/github/androidutils/statemachine/ComplexTransition.java
+++ src/com/github/androidutils/statemachine/ComplexTransition.java
@@ -1,21 +1,9 @@
 package com.github.androidutils.statemachine;
 
-import org.acra.ACRA;
-
 import android.os.Message;
 
-import com.github.androidutils.logger.Logger;
-
 public abstract class ComplexTransition extends State {
-    private final StateMachine sm;
-    private final Logger log;
-
     abstract public void performComplexTransition();
-
-    public ComplexTransition(StateMachine sm, Logger logger) {
-        this.sm = sm;
-        log = logger;
-    }
 
     @Override
     public final void enter() {
@@ -23,12 +11,13 @@ public abstract class ComplexTransition extends State {
     }
 
     @Override
+    public final void resume() {
+        performComplexTransition();
+    }
+
+    @Override
     public final boolean processMessage(Message msg) {
-        log.e("performComplexTransition() must transit immediately");
-        sm.deferMessage(msg);
-        ACRA.getErrorReporter().handleSilentException(
-                new Exception("performComplexTransition() must transit immediately"));
-        return true;
+        throw new RuntimeException("performComplexTransition() must transit immediately");
     }
 
     @Override
